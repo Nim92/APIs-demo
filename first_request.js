846d9034b5a796a4cf29d37fc68495124ba28645
Nim <nim1868@hotmail.com>
2020-06-15 08:21:57 +0000

    added all files

---
+++ first_request.js
@@ -0,0 +1,14 @@
+// var request = require('request');
+// request('http://www.google.com', function(error, response, body) {
+// 	if (!error && response.statusCode == 200) {
+// 		console.log(body) //show the html for google homepage
+// 	}
+// })
+
+const request = require('request');
+request('https://jsonplaceholder.typicode.com/users/1', function (error, response, body){
+	if(!error && response.statusCode == 200){
+		const parsedData = JSON.parse(body);
+		console.log(`${parsedData.name} lives in ${parsedData.address.city} Her phone # is: ${parsedData.phone}`);
+	}
+});
